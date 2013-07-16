9bfc844d23b1ff0808380f5e2848d80214c57b5d
Caio Vaccaro <caiovaccaromora@gmail.com>
2013-07-16 18:34:22 -0300

    Tests

--- messageCenter.js
+++ messageCenter.js
@@ -5,7 +5,7 @@
      * @param  {object} options:
      * {
      *   @param {string} element
-     *   @param {int} timeToClose
+     *   @param {number} timeToClose
      * }
      * @return {void}
      */
@@ -22,12 +22,12 @@
          * Delegate action
          * @param {object} params:
          * { 
-         *   @param {string or array} message: message to be shown
+         *   @param {string|array} message: message to be shown
          *   @param {string} status: css class added, can be 'success', 'warning', 'error' depending on the theme
-         *   @param {string or array} dismiss: class or id of element to trigger closing when clicked, or array of classes/ids, or string 'time' to close by itself. Optional
+         *   @param {string|array} dismiss: class or id of element to trigger closing when clicked, or array of classes/ids, or string 'time' to close by itself. Optional
          *   @param {function} callback: function executed after showing up. Optional
-         *   @param {bool} breaklines: if true add <br> between itens of messages array. Optional
-         *   @param {bool} close, if false don't show close link. Optional
+         *   @param {boolean} breaklines: if true add <br> between itens of messages array. Optional
+         *   @param {boolean} close, if false don't show close link. Optional
          * }
          * @return {void}
          */
@@ -115,10 +115,9 @@
 
             $(this.messageCenter).slideUp(function() {
 
-                $(this).find('p').remove();
-                $(this).removeClass();
+                $(this).find('p').remove().end().removeClass();
 
-                if(open === true) {
+                if(open) {
                     obj.toOpen(params);
                 }
 
@@ -137,9 +136,9 @@
 
             if(dismiss === 'time') {
 
-                    setTimeout(function() {
-                         obj.toClose();
-                    }, obj.timeToClose)
+                setTimeout(function() {
+                     obj.toClose();
+                }, obj.timeToClose)
 
             } else if(dismiss instanceof Array) {
 
---
+++ tests/spec/messageCenter.js
@@ -0,0 +1,66 @@
+'use strict';
+
+var should = chai.should(),
+	msg = new MessageCenter();
+	
+describe('MessageCenter', function() {
+	it('should be an function', function() {
+		MessageCenter.should.be.an('function');
+	});
+
+	describe('New object', function() {
+		it('should be an object', function() {
+			msg.should.be.an('object');
+		});
+		it('should be instance of MessageCenter', function() {
+			msg.should.be.an.instanceOf(MessageCenter);
+		});
+	});
+
+	describe('Default settings', function() {
+		it('messageCenter should be a string', function() {
+			msg.messageCenter.should.be.a('string');
+		});
+		it('timeToClose should be a number', function() {
+			msg.timeToClose.should.be.a('number');
+		});
+	});
+
+	describe('methods', function() {
+		describe('displayMessage', function() {
+			beforeEach(function() {
+				spyOn(msg, 'toOpen');
+			});
+			it('should throw error when message or status are undefined', function() {
+				function message() {
+					msg.displayMessage({ status: 'success' });
+				}
+				function status() {
+					msg.displayMessage({ message: 'Hello!' });
+				}
+				message.should.throw(/MessageCenter: You need to pass the message and status parameter/);
+				status.should.throw(/MessageCenter: You need to pass the message and status parameter/);
+			});
+			it('should call toOpen when required parameters are defined', function() {
+				var params = { message: 'Hello!', status: 'success' };
+				msg.displayMessage(params);
+				expect(msg.toOpen).toHaveBeenCalledWith(params);
+				msg.toClose();
+			});
+		});
+
+		describe('toOpen', function() {
+			beforeEach(function() {
+				msg.toClose();
+				spyOn(msg, 'toClose');
+				spyOn(msg, 'dismiss');
+			});
+			it('should call toClose when MessageCenter is visible and call dismiss when it is defined', function() {
+				msg.displayMessage({ message: 'Hello!', status: 'success', dismiss: 'time' });
+				msg.displayMessage({ message: 'Hello!', status: 'success', dismiss: 'time' });
+				expect(msg.toClose).toHaveBeenCalled();
+				expect(msg.dismiss).toHaveBeenCalled();
+			});
+		});
+	});
+});
